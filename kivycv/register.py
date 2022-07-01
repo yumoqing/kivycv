7d7f4ac4727f4f687e0597b41761b94d5b89b7de
yumoqing <yumoqing@gmail.com>
2022-07-01 11:37:36 +0800

    bugfix

--- kivycv/register.py
+++ kivycv/register.py
@@ -1,13 +1,13 @@
 from kivy.factory import Factory
 
-from audio import Audio
-from audiorecorder import  AudioRecorder
-from camerawithmic import CameraWithMic
-from custom_camera import CustomCamera, QrReader
-from kivycamera import KivyCamera
-from micphone import Micphone
-from qrcodereader import QRCodeReader
-from version import __version__
+from .audio import Audio
+from .audiorecorder import  AudioRecorder
+from .camerawithmic import CameraWithMic
+from .custom_camera import CustomCamera, QrReader
+from .kivycamera import KivyCamera
+from .micphone import Micphone
+from .qrcodereader import QRCodeReader
+from .version import __version__
 r = Factory.register
 r('Audio', Audio)
 r('AudioRecorder', AudioRecorder)
